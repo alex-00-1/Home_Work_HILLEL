355ea6dd3e2b3d1d17a37df9f393a43aeb0528da
Alex <zykoleksandr08@gmail.com>
2025-08-23 14:35:54 +0300

    add 1 to int

---
+++ HW_8.1.py
@@ -0,0 +1,23 @@
+# ДЗ 8.1. Додати 1 до числа
+
+
+def add_one(some_list: list) -> list:
+
+    lst_str = [str(i) for i in some_list]
+    
+    add_numbers = int("".join(lst_str)) + 1
+    numbers_lst = list(str(add_numbers))
+
+    lst_int = [int(i) for i in numbers_lst ]
+    
+    return lst_int
+
+
+# print(add_one([1, 2, 3, 4]))
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1' 
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2' 
+assert add_one([0]) == [1], 'Test3' 
+assert add_one([9]) == [1, 0], 'Test4' 
+
+print("ОК")
